8f9aa147a205bfd83e3907326d35991e2bc531db
ankit <ankipundir090@gmail.com>
2026-03-12 22:41:06 +0530

    setup mediasoup config

--- server/src/lib/ws.ts
+++ server/src/lib/ws.ts
@@ -1,6 +1,13 @@
 import WebSocket from "ws";
+import { createWorker } from "./worker";
 
+let mediasoupRouter;
 const WebsocketConnection = async (websock: WebSocket.Server) => {
+  try {
+    mediasoupRouter = await createWorker();
+  } catch (error) {
+    throw error;
+  }
   websock.on("connection", (ws: WebSocket) => {
     console.log("New WebSocket connection established.");
 
@@ -8,8 +15,7 @@ const WebsocketConnection = async (websock: WebSocket.Server) => {
       console.log(`Received message: ${message}`);
       ws.send(`Echo: ${message}`);
     });
-
-    });
+  });
 };
 
 export { WebsocketConnection };
